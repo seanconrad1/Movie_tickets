e50b025d149a0e5e3001cb8b9a75e6c9c2897342
Jack Yeung <jyeung.se@gmail.com>
2018-07-25 17:56:23 -0400

    Updated controller and views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,7 +16,21 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  def show
+    @movie = Movie.find(params[:id])
+    render :show
+  end
 
+  def edit
+    @movie = Movie.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to movie_path(@movie)
+  end
 
 private
   def movie_params
